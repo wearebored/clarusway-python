04ed1930919cb1c565ead8f0e88c61933781ae23
enes <enesooo78@gmail.com>
2022-12-10 17:57:45 +0300

    tw

---
+++ Teamwork/TW-10.12.22.py
@@ -0,0 +1,38 @@
+# liste=[]
+# elemansay=int(input("eleman sayısı: "))
+# for i in range(elemansay):
+#     print(f"{i+1}. sayıyı giriniz:")
+#     liste.append(int(input()))
+
+# counter=liste[0]
+# for i in liste:
+   
+#     if i>=counter:
+#         counter=i
+
+# print(f"girilen en büyük sayı: {counter}")    
+
+
+# strs = ["eat", "tea", "tan", "ate", "nat", "bat", "cat", "tac"]
+# siralama=[]
+# kelime=[]
+# for i in strs:
+#     liste = "".join(sorted(list(i)))
+#     inde=kelime.count(liste)
+#     if inde>0:
+#         sira=kelime.index(liste)
+#         siralama[sira].append(i)
+#     else:
+#         kelime.append(liste)
+#         siralama.append([i])
+    
+   
+# print(siralama)
+
+            
+    
+    # if siralama[liste]:
+    #     siralama[liste].append(i)
+    # else:
+    #     siralama.pop(liste)
+   
